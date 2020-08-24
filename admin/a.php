480872b6dbd68bab08f71205935c3fb481525795
huynt99 <huyng9x157@gmail.com>
2020-08-24 18:11:08 +0700

    xoa

--- admin/a.php
+++ admin/a.php
@@ -5,40 +5,6 @@ include('../function.php');
 
 ?>
 <?php
-// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//     $error = array();
-
-//     if (empty($POST['category'])) {
-//         $error[] = 'category';
-//     } else {
-//         $cate_name = mysqli_real_escape_string($con, strip_tags($POST['category']));
-//     }
-
-//     if (isset($POST['position']) && filter_var($_GET['cid'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
-//         $position = $POST['position'];
-//     } else {
-//         $error[] = 'position';
-//     }
-
-//     if (empty($error)) {
-//         //neu khong co loi xay ra thi them vao csdl
-//         $que = "UPDATE `categories` SET cate_name = '{$cate_name}', position = $position";
-//         $que .= " WHERE cat_id = {$cid} LIMIT 1";
-//         $res = mysqli_query($con, $que);
-//         confirm_query($res, $que);
-
-//         if ($mysqli_affected_rows($con) == 1) {
-//             $messages =  "<p class='success'>The categories was edited successfully</p>";
-//         } else {
-//             $messages = "<p class='warning'>Could not edit to the categories</p>";
-//         }
-//     } else {
-//         var_dump($error);
-//         $messages = "<p class='warning'>Please fill all required fields</p>";
-//     }
-// }
-
-
 
 $cid = $_GET['cid'];
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
