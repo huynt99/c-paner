c4da3ca451580777584f4e221a63e62c1630ef5b
huynt99 <huyng9x157@gmail.com>
2020-08-25 18:43:38 +0700

    single.php

--- function.php
+++ function.php
@@ -16,3 +16,15 @@ function confirm_query($res, $que)
         die("Query: {$que} \n<br/>  Error: " . mysqli_error($con));
     }
 }
+
+function beauty_text($text){
+    return substr($text, 0, strrpos($text, ' '));
+}
+
+function validate_id($cid){
+    if(isset($cid) && filter_var($cid, FILTER_VALIDATE_INT, array('min_range' => 1))){
+        return $val = $cid;
+    } else {
+        return NULL;
+    }
+}
--- single.php
+++ single.php
@@ -6,28 +6,29 @@ include('views/sidebar-a.php');
 ?>
 <div id="content">
     <?php
-    if (isset($_GET['cid']) && filter_var($_GET['cid'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
-        $cid = $_GET['cid'];
-        $que = "SELECT p.page_name, p.page_id, LEFT(p.content, 400) as content, ";
+    if ($pid = validate_id($_GET['pid'])) {
+        $que = "SELECT p.page_name, p.page_id, p.content, ";
         $que .= " DATE_FORMAT(p.post_on, '%b %d %y') AS date, ";
         $que .= " CONCAT_WS(' ' , u.first_name, u.last_name) AS name, u.user_id ";
         $que .= " FROM pages AS p ";
         $que .= " INNER JOIN users AS u ";
         $que .= " USING (user_id) ";
-        $que .= " WHERE p.cat_id = {$cid} ";
+        $que .= " WHERE p.page_id = {$pid} ";
         $que .= " ORDER BY date LIMIT 0, 10";
         $res = mysqli_query($con, $que);
         confirm_query($res, $que);
         if (mysqli_num_rows($res)) {
             while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
                 echo "<div class='post'>
-                            <H2><a href='single.php?pid={$row['page_id']}'>{$row['page_name']}</a></h2>
-                            <p>".beauty_text($row['content'])." ... <a href='single.php?pid={$row['page_id']}'>Read more</a></p>
+                            <H2>{$row['page_name']}</h2>
+                            <p>{$row['content']}</a></p>
                             <p class='meta'><strong>Post by</strong>: {$row['name']} <strong>On</strong>: {$row['date']}</p>
                         </div>
                         ";
             }
         }
+    } else {
+        redirect_to();
     }
     ?>
 </div>
